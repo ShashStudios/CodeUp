9ab0cfa386a1b0997b8d0634b531ae3bca123fab
recursivebanana <officialtmbrice@gmail.com>
2024-08-02 13:11:00 -0400

    first commit to add work area with questions

--- app/(quesiton)/layout.tsx
+++ app/(quesiton)/layout.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, Box } from '@chakra-ui/react';
 import theme from '../theme'; // Assuming theme.js exists
 import QuestionOne from './QuestionOne/page'; // Adjust import path as needed
 
+
 const RootLayout: React.FC = () => {
   return (
     <ChakraProvider theme={theme}>
